517c452cd073b318fadbc6dea91da8a5a7ea38fe
Hiro <laciferin@gmail.com>
2023-06-24 17:34:05 +0530

    wip: upload file
    Took 43 minutes

--- routers/chat.py
+++ routers/chat.py
@@ -1,35 +1,62 @@
-from fastapi import APIRouter
+import dataclasses
+import http
+from http.client import HTTPException
+from typing import List
+
+from fastapi import APIRouter, status, UploadFile
+from fastapi.responses import JSONResponse
+from h11 import Response
 from pydantic import BaseModel
 
 router = APIRouter(tags=['chat'])
 
 
 class Message(BaseModel):
-    message: str
-    chat_id: int
+    message: str = None
+    chat_id: int = None
+    attachments: UploadFile = None
 
 
 @router.get('/')
-async def get_list():
+async def get_list():  # TODO:
     return {'chats': []}
 
 
-@router.get('/{chat_id}')
+@router.get('/{chat_id}')  # TODO:
 async def get(chat_id: int):
     return {'message': f'chat id {chat_id}'}
 
 
-@router.post('/')
+@router.post('/')  # TODO:
 async def create(msg: Message):
-    new_chat_id: int = 1
-    return {'message': f'chat created id-{new_chat_id}'}
+    new_chat_id: int = msg.chat_id or '1'
+    answer = Message(msg.message, new_chat_id)
+
+    return {'message': answer}
 
 
-@router.put('/{chat_id}')
-async def update(chat_id: int, msg: Message):
-    return {'message': f'chat {chat_id} updated'}
+@router.post('/upload')
+async def upload(msg: Message):
 
+    if msg.attachments is None or msg.chat_id is None:
+        res = JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={'message': 'no file attached or chat_id is not provided'},
+        )
+        return res
 
-@router.delete('/{chat_id}')
-async def delete(chat_id: int):
-    return {'message': f'chat id {chat_id} deleted'}
+    file = msg.attachments
+
+    if file.content_type != 'application/pdf':
+        return {'error': 'Only PDF files are allowed'}
+
+    return {'message': 'uploaded successfully', 'chat_id': msg.chat_id}
+
+# @router.put('/{chat_id}') #TODO:
+# async def upsert(chat_id: int, msg: Message):
+#     return {'message': f'chat {chat_id} updated'}
+#
+#
+# @router.delete('/{chat_id}')  # TODO:
+# async def delete(chat_id: int):
+#     return {'message': f'chat id {chat_id} deleted'}
