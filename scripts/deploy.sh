f03ecc1f0755e7d1e6fd15be6a6ce6558b5c44fb
Hiro <laciferin@gmail.com>
2023-06-28 17:42:13 +0530

    scripts: deploy via ssh
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 3 minutes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,6 @@ session=app
 tmux select-pane -t $session:0
 tmux kill-session -t $session
 tmux new-session -d -s $session -c aichatsback
+tmux select-pane -t $session:0
 command="git pull && python3 -m uvicorn app:app --port 1605 "
 tmux send-keys "$command" Enter
