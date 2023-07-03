5fedfffad29a5a54ff6edbd4a662de394574c5f0
Hiro <laciferin@gmail.com>
2023-07-04 05:02:51 +0530

    scripts: python-anywhere use less workers to get outside tarpit
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 2 minutes

--- scripts/python-anywhere.sh
+++ scripts/python-anywhere.sh
@@ -2,7 +2,7 @@ python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
 pip install uvicorn
-uvicorn app:app --port 1605 --workers 4
+uvicorn app:app --port 1605 --workers 2
 
 #check if the server supports the pinecone env
 # If u r getting max retries  for pinecone api connect
