0e0fd8a339a10c813919259e4a412798b740fc2b
Hiro <laciferin@gmail.com>
2023-07-04 05:00:33 +0530

    scripts: python-anywhere.sh
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 12 minutes

---
+++ scripts/python-anywhere.sh
@@ -0,0 +1,12 @@
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+pip install uvicorn
+uvicorn app:app --port 1605 --workers 4
+
+#check if the server supports the pinecone env
+# If u r getting max retries  for pinecone api connect
+#urllib3.exceptions.MaxRetryError: HTTPSConnectionPool(host='controller.asia-southeast1-gcp-free.pinecone.io', port=443): Max retries exceeded with url: /databases (Caused by NewCon
+# try ping controller.asia-southeast1-gcp-free.pinecone.io
+# check the ip location with curl ipinfo.io
+# delete and create new project for this location
