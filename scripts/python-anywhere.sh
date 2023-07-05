7422c99adbf5038517cd09647503f374909f374d
Hiro <laciferin@gmail.com>
2023-07-05 06:54:13 +0530

    ci: aicommit concurrency
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 6 minutes

--- scripts/python-anywhere.sh
+++ scripts/python-anywhere.sh
@@ -1,8 +1,10 @@
 python3 -m venv venv
 . venv/bin/activate
 pip install --user -r requirements.txt
-pip install uvicorn
-uvicorn app:app --port 1605 --workers 2
+pip install uvicorn gunicorn
+#uvicorn app:app --port 1605 --workers 2
+gunicorn app:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
+
 
 #check if the server supports the pinecone env
 # If u r getting max retries  for pinecone api connect
